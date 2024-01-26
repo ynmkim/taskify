fbaf24a6f57456be25de6ca8f8009e8407199ae6
김동빈 <dongbin@gimdongbin-ui-noteubug.local>
2024-01-26 23:51:33 +0900

    feat: dashboard modal, header ui

--- src/components/header/dashboardHeader.tsx
+++ src/components/header/dashboardHeader.tsx
@@ -6,6 +6,7 @@ import GroupAvatar from '@/components/ui/avatarGroup';
 import crownIcon from '../../../public/assets/icons/crown.svg';
 import settingsIcon from '../../../public/assets/icons/settings.svg';
 import addBoxIcon from '../../../public/assets/icons/add_box.svg';
+import vector from '../../../public/assets/icons/vector.svg';
 
 
 // 나중에 api연동하면 삭제될 임시 데이터입니다.
@@ -37,7 +38,7 @@ const members = [
     id: 3,
     userId: 3,
     email: 'example3@example.com',
-    nickname: 'KimDongbin',
+    nickname: 'dongbin',
     profileImageUrl: '',
     createdAt: '2024-01-25T10:21:46.244Z',
     updatedAt: '2024-01-25T10:21:46.244Z',
@@ -55,7 +56,7 @@ const members = [
     id: 5,
     userId: 5,
     email: 'example3@example.com',
-    nickname: 'dongbin',
+    nickname: 'Kimdongbin',
     profileImageUrl: '',
     createdAt: '2024-01-25T10:21:46.244Z',
     updatedAt: '2024-01-25T10:21:46.244Z',
@@ -63,25 +64,40 @@ const members = [
   }
 ];
 
-const Header:React.FC = () => {
+
+const Header: React.FC<{ columnName: string, type?: string }> = ({ columnName, type }) => {
+  const isDashboard = type === 'myDashboard';
   const totalCount = members.length;
 
+  const getTitle = () => (isDashboard ? '내 대시보드' : columnName);
+
   return (
-    <header>
-      <div className="container flex items-center w-[1620px]">
-        <div className="flex flex-row align-middle font-bold text-xl gap-2">
-          비브리지 <Image src={crownIcon} alt="crown" width={20} height={20} />
+    <header className='w-[1350px] float-right mr-[80px]'>
+      <div className="flex flex-row items-center justify-between h-[70px]">
+        <div className="flex items-center font-bold text-xl gap-2 ml-[40px]">
+          {getTitle()}
+          {!isDashboard && <Image src={crownIcon} alt="crown" width={20} height={20} />}
         </div>
-        <nav className="space-x-4">
-          <Button>
-            <Image src={settingsIcon} alt="settings" width={16} height={16} /> 관리
-          </Button>
-          <Button>
-            <Image src={addBoxIcon} alt="add_box" width={16} height={16} /> 초대하기
-          </Button>
-        </nav>
-        <GroupAvatar members={members} totalCount={totalCount} />
-        <Avatar {...userData} />
+        <div className='flex flex-row items-center'>
+          {!isDashboard && (
+            <nav className="flex flex-row items-center gap-4 mr-[40px]">
+              <Button className='text-gray-787486 flex align-middle gap-2 w-[88px]'>
+                <Image src={settingsIcon} alt="settings" width={20} height={20} /> 관리
+              </Button>
+              <Button className='text-gray-787486 flex align-middle gap-2 w-[116px]'>
+                <Image src={addBoxIcon} alt="add_box" width={20} height={20} /> 초대하기
+              </Button>
+            </nav>
+          )}
+          {!isDashboard && (
+            <>
+              <GroupAvatar members={members} totalCount={totalCount} />
+              <Image src={vector} alt="vector" width={0} height={29} className='mx-[32px]' />
+            </>
+          )}
+          <Avatar {...userData} />
+          <span className="ml-[12px] font-medium text-base">{userData.nickname}</span>
+        </div>
       </div>
     </header>
   );
