fe917ad54d91eb73a43551832f808c8bdd161cbe
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2024-01-28 04:32:21 +0900

    Feat: Dashbard에 헤더 합치고 헤더의 SlotSection 컴포넌트 분리

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,17 +1,21 @@
 import AddColumnButton from "@/components/domains/dashboard/column/AddColumnButton";
 import SideBar from "@/components/domains/dashboard/sidebar/SideBar";
+import DashboardHeader from "@/components/header/DashboardHeader";
 import Column from "@/containers/Column";
 
 export default function DashboardPage() {
   return (
     <div className="flex">
       <SideBar />
-      <div className="flex flex-col lg:flex-row">
-        <Column />
-        <Column />
-        <Column />
-        <AddColumnButton />
-      </div>
+      <main className="flex flex-col max-w-[100vw]">
+        <DashboardHeader columnName="비브리지" />
+        <div className="flex flex-col lg:flex-row w-full">
+          <Column />
+          <Column />
+          <Column />
+          <AddColumnButton />
+        </div>
+      </main>
     </div>
   );
 }
