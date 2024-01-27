209733603883576fe52075106eaad7dd8986ce59
KimByeongHwa <iambyeonghwa@gmail.com>
2024-01-28 02:33:14 +0900

    feat: 회원가입, 로그인 페이지 UI 완성(반응형 적용)

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,6 @@
 import Image from 'next/image';
 import CustomInput from '@/components/auth/input/CustomInput';
 import { InputType } from '@/../type';
-import { Checkbox } from '@/components/ui/checkbox';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 
@@ -14,8 +13,7 @@ export default function SignUp() {
             <Image src="/logo_main.svg" alt="main-logo" priority fill />
           </div>
         </Link>
-
-        <p className="mt-2.5 w-fit mx-auto my-0 text-xl font-medium">첫 방문을 환영합니다!</p>
+        <p className="mt-2.5 w-fit mx-auto my-0 text-xl font-medium">오늘도 만나서 반가워요!</p>
       </div>
 
       <div className="flex flex-col items-center w-[351px] sm:w-[520px]">
@@ -28,42 +26,21 @@ export default function SignUp() {
           />
 
           <CustomInput
-            inputType={InputType.Nickname}
-            id="nickname"
-            labelContext="닉네임"
-            placeholder="닉네임을 입력해 주세요."
-          />
-
-          <CustomInput
             inputType={InputType.Password}
             id="password"
             labelContext="비밀번호"
-            placeholder="8자 이상 입력해 주세요."
-          />
-
-          <CustomInput
-            inputType={InputType.Password}
-            labelContext="비밀번호 확인"
-            placeholder="비밀번호를 한 번 더 입력해 주세요."
+            placeholder="비밀번호를 입력해 주세요."
           />
         </form>
 
-        <div className="mt-6 w-full flex items-center justify-start">
-          <Checkbox
-            id="agrrement"
-            className="mr-2 w-5 h-5 border border-gray-D9D9D9 rounded data-[state=checked]:bg-violet-5534DA"
-          />
-          <label htmlFor="agrrement">이용약관에 동의합니다.</label>
-        </div>
-
         <Button className="mt-5 w-full bg-gray-9FA6B2 text-lg text-white font-medium py-3 hover:bg-violet-5534DA">
-          가입하기
+          로그인
         </Button>
 
         <div className="mt-7 mb-44">
-          이미 가입하셨나요?{' '}
-          <Link href="/login" className="text-violet-5534DA underline">
-            로그인하기
+          회원이 아니신가요?{' '}
+          <Link href="/signup" className="text-violet-5534DA underline">
+            회원가입하기
           </Link>
         </div>
       </div>
