999c35841da20ab5a52f3b50dd9e4b927da5ccec
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2024-01-30 16:49:01 +0900

    Fix: 모달 오류 수정

--- src/components/modal/column.tsx
+++ src/components/modal/column.tsx
@@ -48,37 +48,35 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, title, label, placeholde
     <>
       {isOpen && (
         <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-10">
-          <div className="bg-white p-6 w-[540px] h-[276px] flex-shrink-0 rounded-md">
-            <h2 className="text-2xl font-bold mb-4">{title}</h2>
-            <label className="text-lg font-medium block mb-[10px] mt-[32px]">{label}</label>
-            <input
-              type="text"
-              className="w-full border border-gray-300 rounded-lg px-3 py-3 placeholder:text-gray-500 focus:outline-violet-500"
-              value={inputValue}
-              placeholder={placeholder}
-              onChange={(e) => setInputValue(e.target.value)}
-            />
-            <div className={`mt-[28px] flex items-end ${modalType === 'delete' ? 'justify-between' : 'justify-end'}`}>
-              {modalType === 'delete' && (
-                <button className="text-[#9FA6B2] font-normal text-sm underline" onClick={handleDeleteClick}>
-                  삭제하기
-                </button>
-              )}
-              <div>
-                <Button variant="default" size="modal" text="modal" onClick={onClose} className="w-[120px] h-[48px] py-[14px] px-[46px] mr-[12px]">
-                  취소
-                </Button>
-                <Button variant="violet" size="modal" text="login" onClick={handleConfirm} className="w-[120px] h-[48px] py-[14px] px-[46px]">
-                  {confirmButtonText}
-                </Button>
+          {
+            !isDeleteModalOpen ? (<div className="bg-white p-6 w-[540px] h-[276px] flex-shrink-0 rounded-md">
+              <h2 className="text-2xl font-bold mb-4">{title}</h2>
+              <label className="text-lg font-medium block mb-[10px] mt-[32px]">{label}</label>
+              <input
+                type="text"
+                className="w-full border border-gray-300 rounded-lg px-3 py-3 placeholder:text-gray-500 focus:outline-violet-500"
+                value={inputValue}
+                placeholder={placeholder}
+                onChange={(e) => setInputValue(e.target.value)}
+              />
+              <div className={`mt-[28px] flex items-end ${modalType === 'delete' ? 'justify-between' : 'justify-end'}`}>
+                {modalType === 'delete' && (
+                  <button className="text-[#9FA6B2] font-normal text-sm underline" onClick={handleDeleteClick}>
+                    삭제하기
+                  </button>
+                )}
+                <div>
+                  <Button variant="default" size="modal" text="modal" onClick={onClose} className="w-[120px] h-[48px] py-[14px] px-[46px] mr-[12px]">
+                    취소
+                  </Button>
+                  <Button variant="violet" size="modal" text="login" onClick={handleConfirm} className="w-[120px] h-[48px] py-[14px] px-[46px]">
+                    {confirmButtonText}
+                  </Button>
+                </div>
               </div>
-            </div>
-          </div>
+            </div>) :  <DeleteModal isOpen={isDeleteModalOpen} onClose={handleDeleteCancel} />
+          }
         </div>
-      )}
-
-      {isDeleteModalOpen && (
-        <DeleteModal isOpen={isDeleteModalOpen} onClose={handleDeleteCancel} />
       )}
     </>
   );
