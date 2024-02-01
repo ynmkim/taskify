4627287311f3c052dcf0643e812784d1907cc290
KimByeongHwa <iambyeonghwa@gmail.com>
2024-02-01 15:45:09 +0900

    feat: 랜딩 페이지 파일 구조 생성

---
+++ src/components/home/Point.tsx
@@ -0,0 +1,8 @@
+export default function Point() {
+  return (
+    <div>
+      <div>PointOne</div>
+      <div>PointTwo</div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,19 @@
+import Header from '@/components/home/Header';
+import Footer from '@/components/home/Footer';
+import Hero from '@/components/home/Hero';
+import Point from '@/components/home/Point';
+import Settings from '@/components/home/Settings';
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header />
+      <div>
+        <Hero />
+        <Point />
+        <Settings />
+      </div>
+      <Footer />
+    </div>
+  );
 }
