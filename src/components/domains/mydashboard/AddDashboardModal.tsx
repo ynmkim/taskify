d319d9cbd1482b0e53e9065e7bc1e0e36972988b
김윤미 <younmeet@gmail.com>
2024-02-04 00:53:46 +0900

    Fix: ui input-base 분리

--- src/components/domains/mydashboard/AddDashboardModal.tsx
+++ src/components/domains/mydashboard/AddDashboardModal.tsx
@@ -1,7 +1,7 @@
 import DashboardCardTitle from './DashboaordCardTitle';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
+import { Input } from '@/components/ui/base-input';
 import { Label } from '@/components/ui/label';
 
 export default function AddDashboardModal() {
